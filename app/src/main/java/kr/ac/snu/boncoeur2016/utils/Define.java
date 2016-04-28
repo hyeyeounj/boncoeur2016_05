cf3bf91cf3c087a014858293583b17ce0b6f6251
Sunkyue Kim <github@kimsunk.net>
2016-04-28 23:20:53 +0900

    minSDK 17 지원하도록 수정 API LEVEL 17에서는 다른 함수를 사용해서 AAC 형식으로 저장

--- app/src/main/java/kr/ac/snu/boncoeur2016/utils/Define.java
+++ app/src/main/java/kr/ac/snu/boncoeur2016/utils/Define.java
@@ -10,15 +10,11 @@ public class Define {
     public static final String POS_TAG_P = "pulmonic";
     public static final String POS_TAG_M = "mitral";
     public static final String POS_TAG_T = "tricuspid";
-
+    public static final int REVISION = 8888;
+    public static final String RECORDED_FILEPATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/BonCoeur/";
+    public static final int SHORT_TIME = 10000;
+    public static final int PAUSE = 9999;
     public static int TYPE_WIFI = 1;
     public static int TYPE_MOBILE = 2;
     public static int TYPE_NOT_CONNECTED = 0;
-
-    public static final int REVISION = 8888;
-
-    public static final String RECORDED_FILEPATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/BonCoeur/";
-
-    public static final int SHORT_TIME = 5000;
-    public static final int PAUSE = 9999;
 }
