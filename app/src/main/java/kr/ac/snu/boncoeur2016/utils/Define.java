47ed4054df6045addc4120a96ecd26602cf5dad2
kimsunk <github@kimsunk.net>
2016-04-28 23:23:11 +0900

    숏타임 5초 10초로 늘린거 원복

--- app/src/main/java/kr/ac/snu/boncoeur2016/utils/Define.java
+++ app/src/main/java/kr/ac/snu/boncoeur2016/utils/Define.java
@@ -12,7 +12,7 @@ public class Define {
     public static final String POS_TAG_T = "tricuspid";
     public static final int REVISION = 8888;
     public static final String RECORDED_FILEPATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/BonCoeur/";
-    public static final int SHORT_TIME = 10000;
+    public static final int SHORT_TIME = 5000;
     public static final int PAUSE = 9999;
     public static int TYPE_WIFI = 1;
     public static int TYPE_MOBILE = 2;
